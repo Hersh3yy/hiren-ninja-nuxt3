2cb9463e42fcd2524ba9e0ae1ded544b6e0c4805
HirenBudhAir <hirenbudhrani@gmail.com>
2023-10-13 14:19:23 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hiren.ninja
+
+Personal / professional website of Hiren  Budhrani, a developer, music enthusiast, levensgenieter, and child-at-heart
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts,html}",
+    "./layouts/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    './pages/**/*.{html,js,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
